77a6899a43a8d6080e3589dbba9e0b4508ff7ae1
Arsema Addis <arsemadd@gmail.com>
2025-04-10 22:26:14 +0300

    add TodoItem

--- src/main/java/com/example/todolist/model/TodoItem.java
+++ src/main/java/com/example/todolist/model/TodoItem.java
@@ -1,10 +1,29 @@
 package com.example.todolist.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class TodoItem {
 
     private Long id;
     private String title;
     private boolean done;
 
+    public TodoItem() {
+    }
+
+    public TodoItem(Long id, String title, boolean done){
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
+
+    @Id
+    @GeneratedValue
+    public long getId() {
+        return id;
+    }
 
 }
