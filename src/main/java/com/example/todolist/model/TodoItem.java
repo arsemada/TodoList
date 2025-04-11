6a8c02bdb7823052e7a38186d4b473982269bb02
Arsema Addis <arsemadd@gmail.com>
2025-04-11 23:39:47 +0300

    update TodoItem

--- src/main/java/com/example/todolist/model/TodoItem.java
+++ src/main/java/com/example/todolist/model/TodoItem.java
@@ -8,6 +8,8 @@ import jakarta.validation.constraints.NotBlank;
 @Entity
 public class TodoItem {
 
+    @Id
+    @GeneratedValue
     private Long id;
     @NotBlank
     private String title;
@@ -22,10 +24,27 @@ public class TodoItem {
         this.done = done;
     }
 
-    @Id
-    @GeneratedValue
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 }
